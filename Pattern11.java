18b32b139d7c1b03db34b4e4e27e4bad40ae1c60
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-16 06:43:47 +0530

    pattern 11

---
+++ Pattern11.java
@@ -0,0 +1,23 @@
+class Pattern11 {
+    public void pattern11(int n) {
+        int start = 0;
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                start = 1;
+                printPattern(i,start);
+            }
+            else {
+                start = 0;
+                printPattern(i,start);
+            }
+        }
+    }
+    public void printPattern(int i, int start) {
+        while(i+1>0) {
+            System.out.print(start + " ");
+            if(start==1) start = 0; else start = 1;
+            i--;
+        }
+        System.out.println();
+    }
+}
