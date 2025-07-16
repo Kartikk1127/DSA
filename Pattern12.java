361c2adaed70e810f20a0871d485ffd3e9e3c853
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-16 07:32:09 +0530

    pattern 12 and pattern 13

---
+++ Pattern12.java
@@ -0,0 +1,18 @@
+public class Pattern12 {
+    public void pattern12(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            int spaces = 2 * (n - i);
+            while (spaces > 0) {
+                System.out.print(" ");
+                spaces--;
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern13.java
@@ -0,0 +1,12 @@
+public class Pattern13 {
+    public void pattern13(int n) {
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
