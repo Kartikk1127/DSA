c4e8f31f1b84231830348259fa8a00b442473a5d
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-17 07:56:39 +0530

    pattern 18

--- Main.java
+++ Main.java
@@ -1,9 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++) {
-            i=66;
-            char ch = (char) i;
-            System.out.println(ch);
+        pattern19(5);
+    }
+    public static void pattern19(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < 2 * i; k++) {
+                System.out.print(" ");
+            }
+            for (int l = 0; l < n - i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < 2 * (n-i-1); k++) {
+                System.out.print(" ");
+            }
+            for (int l = 0; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
---
+++ Pattern18.java
@@ -0,0 +1,11 @@
+public class Pattern18 {
+    public void pattern18(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                char ch = (char) (n + 65 - j - 1);
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
