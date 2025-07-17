8604f403f1355169d3a1ebc91da3df4e8597e31a
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-17 08:38:58 +0530

    pattern 20

--- Pattern20.java
+++ Pattern20.java
@@ -1,16 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        pattern20(5);
-    }
-    public static void pattern20(int n) {
-        for(int i = 0; i < n; i ++) {
-            for(int j = 0; j <= i; j++) {
+public class Pattern20 {
+    public void pattern20(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
-            for(int k = 0; k < 2 * (n-i-1); k++) {
+            for (int k = 0; k < 2 * (n - i - 1); k++) {
                 System.out.print(" ");
             }
-            for(int l = 0; l <=i; l++) {
+            for (int l = 0; l <= i; l++) {
                 System.out.print("*");
             }
             System.out.println();
