9b7473ee841080c1898741f6f0fbda0551ff1b4d
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-18 06:18:46 +0530

    pattern 21

--- Main.java
+++ Main.java
@@ -1,33 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        pattern20(5);
+        pattern21(5);
     }
-    public static void pattern20(int n) {
-        for(int i = 0; i < n; i ++) {
-            for(int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
-            for(int k = 0; k < 2 * (n-i-1); k++) {
-                System.out.print(" ");
-            }
-            for(int l = 0; l <=i; l++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            if (i == n - 1) {
-                for (int temp = i; temp > 0; temp--) {
-                    for (int a = 0; a < temp; a++) {
+    public static void pattern21(int n) {
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == n - 1) {
+                for (int temp = 0; temp < n; temp++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            } else {
+                for (int j = 0; j < n; j++) {
+                    if (j == 0 || j == n - 1) {
                         System.out.print("*");
-                    }
-                    for (int b = 0; b < 2 * (n - temp); b++) {
+                    } else {
                         System.out.print(" ");
                     }
-                    for (int c = 0; c < temp; c++) {
-                        System.out.print("*");
-                    }
-                    System.out.println();
                 }
-                break;
+                System.out.println();
             }
         }
     }
