59b82b863b026d62c006c0b1afe5b310200ec546
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-25 07:11:58 +0530

    second most frequency

---
+++ hashing/SecondMostFrequency.java
@@ -0,0 +1,32 @@
+package hashing;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SecondMostFrequency {
+    public int secondMostFrequentElement(int[] nums) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> frequency = new LinkedHashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            frequency.put(nums[i], frequency.getOrDefault(nums[i], 0) + 1);
+        }
+        int maxFrequencyElement = -1;
+        int maxFrequencyCount = 0;
+
+        int secondMostFrequentElement = -1;
+        int secondMostFrequentCount = 0;
+        for (Map.Entry<Integer, Integer> mp : frequency.entrySet()) {
+            if (mp.getValue() > maxFrequencyCount) {
+                secondMostFrequentCount = maxFrequencyCount;
+                secondMostFrequentElement = maxFrequencyElement;
+                maxFrequencyElement = mp.getKey();
+                maxFrequencyCount = mp.getValue();
+            } else if (mp.getValue() > secondMostFrequentCount && mp.getValue() < maxFrequencyCount) {
+                secondMostFrequentElement = mp.getKey();
+                secondMostFrequentCount = mp.getValue();
+            }
+        }
+        return secondMostFrequentElement;
+    }
+}
