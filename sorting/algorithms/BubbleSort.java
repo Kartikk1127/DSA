f291e81fea6bf6e80e8e2d0687c566cd03015e1e
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-08-05 06:22:36 +0530

    bubble sort

---
+++ sorting/algorithms/BubbleSort.java
@@ -0,0 +1,16 @@
+package sorting.algorithms;
+
+public class BubbleSort {
+    public int[] bubbleSort(int[] nums) {
+        for(int i = nums.length - 1; i >= 0; i --) {
+            for(int j = 0; j <= i - 1; j ++) {
+                if(nums[j]> nums[j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
