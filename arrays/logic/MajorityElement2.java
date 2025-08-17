17a890d0ab8bcf27b18ef74eed03b3cd3b7ed15b
Kartikey Srivastava <kartikey.srivastava1271@gmail.com>
2025-08-17 23:26:54 +0530

    moore voting algorithm

--- arrays/logic/MajorityElement2.java
+++ arrays/logic/MajorityElement2.java
@@ -10,20 +10,53 @@ public class MajorityElement2 {
         int [] arr = {1, 2, 1, 1, 3, 2};
         System.out.println(majorityElementTwo(arr));
     }
+
+    // most optimal
+    // moore's voting algorithm
     public static List<Integer> majorityElementTwo(int[] nums) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-        int count = 0;
-        for (int num : nums) {
-            if (!frequencyMap.containsKey(num)) {
-                frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-                if (frequencyMap.get(num) > nums.length / 3) {
-                    if(!result.contains(num)) {
-                        result.add(num);
-                    }
-                }
+        int requiredCount = nums.length/3;
+        List<Integer> list = new ArrayList<>();
+        int count1 = 0, count2 =0, element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
+
+        for (int j : nums) {
+            if (count1 == 0 && j != element2) {
+                count1 = 1;
+                element1 = j;
+            } else if (count2 == 0 && j != element1) {
+                count2 = 1;
+                element2 = j;
+            } else if (j == element1) count1++;
+            else if (j == element2) count2++;
+            else {
+                count1--;
+                count2--;
             }
         }
-        return result;
+        int freqElement1 = 0;
+        int freqElement2 = 0;
+        for(int num : nums) {
+            if(num == element1) freqElement1++;
+            else if(num == element2) freqElement2++;
+        }
+        if(freqElement1>requiredCount) list.add(element1);
+        if(freqElement2>requiredCount) list.add(element2);
+        return list;
     }
+    //less optimal
+//    public static List<Integer> majorityElementTwo(int[] nums) {
+//        Map<Integer, Integer> frequencyMap = new HashMap<>();
+//        List<Integer> result = new ArrayList<>();
+//        int count = 0;
+//        for (int num : nums) {
+//            if (!frequencyMap.containsKey(num)) {
+//                frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+//                if (frequencyMap.get(num) > nums.length / 3) {
+//                    if(!result.contains(num)) {
+//                        result.add(num);
+//                    }
+//                }
+//            }
+//        }
+//        return result;
+//    }
 }
