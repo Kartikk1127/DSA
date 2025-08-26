be0129d3c02eb3f9ccb8fc802daeaeff4c9e92b2
Kartikey Srivastava <kartikey.srivastava1271@gmail.com>
2025-08-26 05:57:59 +0530

    sum of highest and lowest frequency

--- hashing/basic/SumHighestAndLowestFrequency.java
+++ hashing/basic/SumHighestAndLowestFrequency.java
@@ -4,7 +4,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SumHighestAndLowestFrequency {
-    public int sumHighestAndLowestFrequency(int[] nums) {
+    public static void main(String[] args) {
+        int [] arr = {5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000,5000};
+        System.out.println(sumHighestAndLowestFrequency(arr));
+    }
+    /*public int sumHighestAndLowestFrequency(int[] nums) {
         Map<Integer, Integer> frequency = new LinkedHashMap<>();
         for (int i = 0; i < nums.length; i++) {
             frequency.put(nums[i], frequency.getOrDefault(nums[i], 0) + 1);
@@ -21,5 +25,31 @@ public class SumHighestAndLowestFrequency {
             }
         }
         return lowest+highest;
+    }*/
+
+    public static int sumHighestAndLowestFrequency(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        int[] freqArray = new int[max + 1];
+        for (int num : nums) {
+            freqArray[num] += 1;
+        }
+        int maxFreq = -1;
+        int minFreq = Integer.MAX_VALUE;
+        for (int i = 0; i < freqArray.length; i++) {
+            if (freqArray[i] > 0) {
+                if (freqArray[i] > maxFreq) {
+                    maxFreq = freqArray[i];
+                }
+                if (freqArray[i] < minFreq) {
+                    minFreq = freqArray[i];
+                }
+            }
+        }
+        return maxFreq + minFreq;
     }
 }
