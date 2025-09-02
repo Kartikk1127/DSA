e1469f774362c4e1dd418dc992748da2d6fbf329
Kartikey Srivastava <kartikey.srivastava1271@gmail.com>
2025-09-02 06:55:45 +0530

    count subarray sum

--- hashing/medium/CountSubarraysWithGivenSum.java
+++ hashing/medium/CountSubarraysWithGivenSum.java
@@ -29,5 +29,20 @@ public class CountSubarraysWithGivenSum {
         return sum;
     }
 
+    // better
+    public static int subarraySumBetter(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+
 
 }
